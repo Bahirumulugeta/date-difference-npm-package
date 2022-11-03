017bb80f91ef9baeae1d4a45e0e609ec0276ca5b
bahiru <bahirumulugeta1@gmail.com>
2022-11-03 15:44:23 +0200

    readme is changed

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function getDifference(date) {  //The function that accept the previous date
     var diffDate = new Date(new Date() - startDate);
     let years = diffDate.toISOString().slice(0, 4) - 1970;
     if (years >= 1) return years + " years ago";
-    else if (diffDate.getMonth() >= 1)
-      return diffDate.getMonth() + " months ago";
+    else if (diffDate.getMonth() >= 1) 
+      return diffDate.getMonth() + " months ago"; 
     else if (diffDate.getDate() - 1 >= 1)
-      return diffDate.getDate() - 1 + " days ago";
+      return diffDate.getDate() - 1 + " days ago"; 
     else if (diffDate.getHours() -2 >= 1)
       return diffDate.getHours() - 2 + " hours ago";
     else if (diffDate.getMinutes() >= 1)
